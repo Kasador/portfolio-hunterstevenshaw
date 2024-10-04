57dfdb380f4150f669b6a3f4a079868a510d221b
Kasador <hunterstevenshaw@gmail.com>
2024-10-04 17:53:47 -0500

    Updated File Structure >> Assets moved to Public folder. // Added Video backgrounds for start.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
-// import logo from './logo.svg';
 import './css/App.css';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    // Detect if the client is on mobile or desktop
+    const userAgent = navigator.userAgent || navigator.vendor || window.opera;
+    
+    if (/android/i.test(userAgent)) {
+      setIsMobile(true);
+    } else if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+      setIsMobile(true);
+    } else {
+      setIsMobile(false);
+    }
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-          ReactJS Portfolio in Progress. 
-        </p>
+        <video autoPlay muted loop className="background-video">
+          <source src={isMobile ? '/assets/video/Mobile_v5_Slow.mp4' : '/assets/video/Desktop.mp4'} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './css/reset.css';
 import './css/index.css';
 import App from './App';
 
