c655be8ebcb68910f8578b25eff374b01a51e686
Kasador <hunterstevenshaw@gmail.com>
2024-10-04 16:50:14 -0500

    Updated File Structure // DevOps

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './dev/index.css';
+import './dev/css/index.css';
 import App from './dev/App';
 import reportWebVitals from './reportWebVitals';
 
