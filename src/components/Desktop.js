60bd6fe25c36835326048f46d164bf91e5776402
Kasador <hunterstevenshaw@gmail.com>
2024-10-05 07:11:01 -0500

    Updated to user XML and not HTML. // class to className // video controls to attribute plus values.

--- src/components/Desktop.js
+++ src/components/Desktop.js
@@ -4,16 +4,12 @@ import '../css/desktop/Desktop.css';
 
 function Desktop() {
   return (
-    <div class="desktop">
-      <video class="video-desktop" autoPlay muted loop>
+    <div className="desktop">
+      <video className="video-desktop" autoPlay muted loop>
         <source src="/assets/video/desktop/Desktop.mp4" type="video/mp4" />
       </video>
-      <div class="desktop-main">
+      <div className="desktop-main">
         {/* Add your Windows 7 emulation code here */}
-        <div class="desktop-time-wrapper">
-          <div class="desktop-time">9:03 PM</div>
-          <div class="desktop-date">10/4/2024</div>
-        </div>
       </div>
     </div>
   );
--- src/components/Mobile.js
+++ src/components/Mobile.js
@@ -4,16 +4,17 @@ import '../css/mobile/Mobile.css';
 
 function Mobile() {
   return (
-    <div class="mobile">
-      <video class="video-mobile" autoPlay muted loop>
+    <div className="mobile">
+      <video className="video-mobile"
+        autoPlay="autoplay"
+        playsInLine="playsinline" 
+        loop="true" 
+        muted="true"
+      >
         <source src="/assets/video/mobile/Mobile_v5_Slow.mp4" type="video/mp4" />
       </video>
-      <div class="mobile-main">
+      <div className="mobile-main">
         {/* Add your iPhone emulation code here */}
-        <div class="mobile-header">
-          <div class="mobile-carrier">5G</div>
-          <div class="mobile-service-battery">Full</div>
-        </div>
       </div>
     </div>
   );
